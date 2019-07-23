49d5f9147c33658286abba0e33e9f555402d784f
ZaL <z4ls@live.com>
2019-07-23 16:33:02 +0400

    remove duplicate yellow box avoid msg

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import {Text, View, YellowBox} from 'react-native';
+import {Text, View} from 'react-native';
 import {createAppContainer, createBottomTabNavigator} from 'react-navigation';
 import Broadcast from './Broadcast';
 import Connect from './Connect';
 
 class HomeScreen extends React.Component {
-
-  constructor(props) {
-    super(props);
-    console.ignoredYellowBox = ['Remote debugger'];
-    YellowBox.ignoreWarnings([
-      'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
-    ]);
-  }
 
   render() {
     return (
